5b5d4e79943e098c841fab3ae3d00302933fb4d9
juan <marinjuanpabloisaza@gmail.com>
2021-09-20 10:02:44 -0500

    entrega 2

--- js/main.js
+++ js/main.js
@@ -26,3 +26,22 @@ modal.addEventListener('click', (event) => {
 
     }
 })*/
+
+
+const btnOpenModalR = document.getElementById('registrarse')
+const modalR = document.getElementById('modalR')
+let isOpenModalR = false
+
+btnOpenModalR.addEventListener('click', (event) => openModalR())
+
+function openModalR() {
+
+    if (isOpenModalR) {
+        modalR.style.display = 'none'
+    } else {
+
+        modalR.style.display = 'flex'
+
+    }
+    isOpenModalR = !isOpenModalR
+}
