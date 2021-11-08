5f5bee1b26425495a52e90f611029798ffe602af
juan <marinjuanpabloisaza@gmail.com>
2021-11-08 16:49:00 -0500

    8 de nov  swiper parte 1

--- Index/main.js
+++ Index/main.js
@@ -2,19 +2,25 @@ const btnOpenModal = document.getElementById('inicioSesion')
 const modal = document.getElementById('modal')
 let isOpenModal = false
 
+fetch('https://randomuser.me/api')
+    .then(response => response.json())
+    .then((json) => {
+        console.log(json)
+        const text =
+            document.getElementById('_name').innerHTML = json.results[0].name.first;
+        document.getElementById('_img').setAttribute("src", json.results[0].picture.large)
+        console.log(document.getElementById('_img').getAttribute("src"))
+    })
+
 btnOpenModal.addEventListener('click', (event) => openModal())
 
 function openModal() {
-
     if (isOpenModal) {
         modal.style.display = 'none'
     } else {
-
         modal.style.display = 'flex'
-
     }
     isOpenModal = !isOpenModal
-
 }
 
 
@@ -36,3 +42,35 @@ function openModalR() {
     }
     isOpenModalR = !isOpenModalR
 }
+
+
+/// swiper
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
